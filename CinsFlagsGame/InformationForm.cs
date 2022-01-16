23b57acb762474d237b47865484dedadc0cea134
yildizemre2021 <yildiz.emre.2021>
2022-01-16 13:33:36 +0300

    some spelling errors corrected.

--- CinsFlagsGame/InformationForm.cs
+++ CinsFlagsGame/InformationForm.cs
@@ -19,14 +19,14 @@ namespace CinsFlagsGame
 
         private void InformationForm_Load(object sender, EventArgs e)
         {
-            listBox1.Items.Add("* First select your five Land.");
+            listBox1.Items.Add("* First select your five Land. But They must be neighboor to one of them.");
             listBox1.Items.Add("");
             listBox1.Items.Add("* Then wait for opponent to finish selecting his/her lands.");
             listBox1.Items.Add("");
             listBox1.Items.Add("* In the second stage of the game, shoot an opponent land which you predict");
             listBox1.Items.Add("  that his/her land.");
             listBox1.Items.Add("");
-            listBox1.Items.Add("* If you shoot fifth of opponents lands firsly, You Win !");
+            listBox1.Items.Add("* If you shoot fifth of opponents lands first, You Win !");
             listBox1.Items.Add("");
             listBox1.Items.Add("* Good Luck !");
             listBox1.Items.Add("");
